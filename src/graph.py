e9c7645118fde179c180c9c72e25949ff324b346
Joao <jpmaresp@gmail.com>
2022-11-23 17:10:50 -0300

    adding comments

--- src/graph.py
+++ src/graph.py
@@ -8,21 +8,21 @@ from matplotlib import pyplot as plt
 
 class Graph():
     
-    def __init__(self):
-        self.edges: list[Edge] = []
-        self.nmNodes : int = None
-        self.G = nx.DiGraph()
-        self.nodes: list[Node] = []
-        self.visitedEdges: list[Edge] = []
-        self.visitedNodes: list[Node] = []
-        self.workload: int = 0
-        self.gain: int = 0
+    def __init__(self):#construtor da classe Grafo
+        self.edges: list[Edge] = [] #lista de arestas do grafo
+        self.nmNodes : int = None #numero de vertices do grafo
+        self.G = nx.DiGraph() #formação do grafo direcionado pela lib NetworkX
+        self.nodes: list[Node] = [] #lista de vertices do grafo
+        self.visitedEdges: list[Edge] = [] #lista de arestas já visitadas no grafo
+        self.visitedNodes: list[Node] = [] #lista de vertices já visitados no grafo
+        self.workload: int = 0 #carga horária
+        self.gain: int = 0 #ganho total
 
-    def addEdge(self, edge : Edge):
+    def addEdge(self, edge : Edge): #adiciona uma aresta no grafo
         self.edges.append(edge)
         self.G.add_edge(edge.begin.id, edge.end.id, length = edge.gas_cost, weight = edge.distance)
 
-    def addNode(self, node : Node):
+    def addNode(self, node : Node): #adiciona um vertice no grafo
         replica = False
         for i in self.nodes:
             if (node.id == i.id):
@@ -32,9 +32,9 @@ class Graph():
         if (not replica):
             self.nodes.append(node)
                 
-    def printGraph(self, nodes):
+    def printGraph(self, nodes): #printa e plota o grafo, utilizando as libs: networkX e matplot
         color_map = []
-        pos = {
+        pos = { #posições dos nós no plano cartesiano
             '0':([0.0, 0.0]),
             '1':([-0.2, 4.0]),
             '2':([5.5, 8.0]),
@@ -47,7 +47,7 @@ class Graph():
             '9':([-2.0, 6.0])            
         }
         
-        for i in self.nodes:
+        for i in self.nodes:#loop que colorirá o grafo, verde os vértices já visitados e azul os não visitados
             if (self.existNode(i, nodes)):
                 color_map.append('green')
             else:
@@ -58,25 +58,22 @@ class Graph():
         nx.draw_networkx_labels(self.G, pos)
         plt.show()
 
-    def setNmNodes(self, nmNodes : int):
+    def setNmNodes(self, nmNodes : int): #define o número de vértices do grafo
         self.nmNodes = nmNodes
 
-    def adjMatrix(self):
-        return nx.to_numpy_matrix(self.G)
-
-    def verifyNode(self, node_id):
+    def verifyNode(self, node_id): #recebe um id de um vértice e verifica se este existe no grafo
         for i in self.nodes:
             if (i.id == node_id):
                 return i
 
-    def existNode(self, node, visitedNodes):
+    def existNode(self, node, visitedNodes): #recebe um id de um vértice e verifica se este existe já foi visitado no grafo
         for i in visitedNodes:
             if (i == node):
                 return True
 
         return False
 
-    def return_VisitedNodeList(self, str):
+    def return_VisitedNodeList(self, str): #retorna uma lista de vértices visitados no grafo
         split = str.split(' ')
         list = []
         for i in split:
@@ -84,25 +81,25 @@ class Graph():
                 list.append(self.verifyNode(i))
         return list
 
-    def verifyEdge(self, edge):
+    def verifyEdge(self, edge): #recebe uma aresta e verifica se esta ja foi percorrida no grafo
         for i in self.visitedEdges:
             if (i == edge):
                 return True
         return False
 
-    def find_BestStep(self, node):
+    def find_BestStep(self, node): #função de caminhamento guloso
         max_bounty = 0
         canContinue = False
 
-        for i in node.edges: 
- 
+        for i in node.edges: #recebe um vértice 'node' e faz-se a seguinte verificação: o ganho do vértice da outra ponta da aresta possui um ganho maior que o já armazenado em 'max_bounty'? Com a carga-horária atual, é possível caminhar para este vértice em observação e voltar para divinópolis? A aresta a ser caminhada ja foi utilizada?
+    
             if (i.end.bounty > max_bounty and i.end.time_spent < (self.workload - self.timeToReturn(i.end) - i.distance) and (not self.verifyEdge(i))):
                 max_bounty = i.end.bounty
                 best_node = i.end
                 edge_passed = i 
                 canContinue = True
 
-        if(canContinue):
+        if(canContinue): #se foi possível dar um passo, adiciona o vértice na lista de visitados e atualiza o ganho e a carga-horária do grafo
             self.visitedNodes.append(best_node)
             node = best_node
             if (edge_passed.begin == self.nodes[0]):
@@ -110,7 +107,7 @@ class Graph():
             self.workload = self.workload - (best_node.time_spent + edge_passed.distance)
             self.gain = self.gain + (best_node.bounty - edge_passed.gas_cost)
         
-        else:
+        else: #se não foi possível dar o passo, volta para Divinópolis e atualiza o ganho e a carga-horária
             self.visitedNodes.append(self.nodes[0])
             self.workload = self.workload - self.timeToReturn(node)
             self.gain = self.gain - self.costToReturn(node)
@@ -118,38 +115,38 @@ class Graph():
         
         return node
 
-    def gulosoMaxValue(self, workload):
-        results_dict = {}
+    def gulosoMaxValue(self, workload): #recebe a carga horária semanal e roda o algoritmo guloso olhando todas as possibildades de caminho, fazendo um intermédio entre programação-dinâmica e guloso
+        results_dict = {} #dicionário que armazenará o resultado final do algoritmo
         results_dict["Nodes"] = ""
         results_dict["Gain"] = self.gain
         
-        for i in range (len(self.nodes[0].edges)):
-            self.cleanConstants(workload)
+        for i in range (len(self.nodes[0].edges)): #enquando houver arestas saindo de divinópolis, rodará a função find_BestStep()
+            self.cleanVariables(workload)
             no = self.nodes[0]
             self.visitedNodes.append(no)
-            if (self.can_ChooseEdge(no)):
+            if (self.can_ChooseEdge(no)): #observa se há carga-horária suficiente para andar no grafo, pois há casos que há algumas arestas que não são possíveis de percorrer
                 while (no != self.find_BestStep(no)):
                     no = self.visitedNodes[-1]
             else:
                 break   
 
             if (self.gain > results_dict["Gain"]):
-                results_dict["Nodes"] = self.printVisitedNodes()
+                results_dict["Nodes"] = self.returnVisitedNodes()
                 results_dict["Gain"] = self.gain
 
         return results_dict
 
-    def timeToReturn(self, node):
+    def timeToReturn(self, node): #recebe um vértice e retorna o tempo necessário para voltar desse vertice para divinopolis
         for i in node.edges:
             if (i.end == self.nodes[0]):
                 return i.distance
 
-    def costToReturn(self, node):
+    def costToReturn(self, node): #recebe um vértice e retorna o custo com gasolina necessário para voltar desse vertice para divinopolis
         for i in node.edges:
             if (i.end == self.nodes[0]):
                 return i.gas_cost
 
-    def printVisitedNodes(self):
+    def returnVisitedNodes(self): #retorna uma string apontando os vértices que foram caminhados
         str = ""
         for i in self.visitedNodes:
             str = str + i.id + " -> "
@@ -157,21 +154,21 @@ class Graph():
         str = str + "home"
         return str
 
-    def printVisitedEdges(self):
+    def printVisitedEdges(self): #printa as arestas visitadas
         for i in self.visitedEdges:
             print(i.begin.id," -> ", i.end.id)
 
-    def addEdge_NodeEdgeList(self, node):
+    def addEdge_NodeEdgeList(self, node): #adiciona uma aresta na lista de arestas visitadas de um vértice específico
         for i in self.edges:
             if (node.id == i.end.id and i.begin == self.nodes[0]):
                 self.visitedEdges.append(i)
 
-    def cleanConstants(self, workload):
+    def cleanVariables(self, workload): #limpa todas as variáveis utilizadas no loop guloso-dinâmico
         self.workload = workload
         self.visitedNodes.clear()
         self.gain = 0
 
-    def can_ChooseEdge(self, node):
+    def can_ChooseEdge(self, node): #retorna se é possível percorrer por alguma aresta no grafo
         for i in node.edges:
             if (i.end.time_spent < (self.workload - self.timeToReturn(i.end) - i.distance) and (not self.verifyEdge(i))):
                 return True
--- src/process_file.py
+++ src/process_file.py
@@ -2,21 +2,21 @@ from edge import Edge
 from node import Node
 from graph import Graph
 
-def processFile(file, graph):
+def processFile(file, graph): #separa o arquivo por linhas
     for line in file:
         processLine(line, graph)
 
-def processLine(line: str, graph):
+def processLine(line: str, graph): #separa o arquivo removendo espaços entre strings
     split = line.split(' ')
     getNumberOfNodes(split, graph)
     buildEdge(split, graph)
 
-def getNumberOfNodes(split, graph):
+def getNumberOfNodes(split, graph): #pesquisa no arquivo o número de vértices do grafo
     if (split[0] == "Vertices"):
         nmNodes = split[2].replace('\n', '')
         graph.setNmNodes(int(nmNodes))
 
-def getFloatValueFromTitle(line, title):
+def getFloatValueFromTitle(line, title): #retorna como float dado uma string do arquivo removendo possíveis '\n' no final da string
         value_index = line.index(title) + 1
         value = line[value_index]
         try :
@@ -26,20 +26,20 @@ def getFloatValueFromTitle(line, title):
             return float(value)
 
 
-def buildEdge(split, graph):
+def buildEdge(split, graph): #constrói uma aresta de acordo com os dados lidos no arquivo
     if (split[0][0] == '('):
         nodes = split[0][1:-1].split(',')
         for edge_info in split:
             if (edge_info == "distance"):
-                distance = getFloatValueFromTitle(split, edge_info)
+                distance = getFloatValueFromTitle(split, edge_info) #pega a distancia
 
             if (edge_info == "gas_cost"):
-                gas_cost = getFloatValueFromTitle(split, edge_info)
+                gas_cost = getFloatValueFromTitle(split, edge_info) #pega o custo de gasolina
 
 
 
-        begin = verifyNode(nodes[0], graph)
-        end = verifyNode(nodes[1], graph)
+        begin = verifyNode(nodes[0], graph) #adiciona o vértice inicial da aresta
+        end = verifyNode(nodes[1], graph) #adiciona o vértice final da aresta
 
         edge = Edge(
             gas_cost,
@@ -55,7 +55,7 @@ def verifyNode(node_id, graph):
         if (i.id == node_id):
             return i
 
-def buildNode(file, graph):
+def buildNode(file, graph): #constrói um vértice de acordo com os dados lidos no arquivo
     for line in file:
         split = line.split(' ')
     
@@ -63,10 +63,10 @@ def buildNode(file, graph):
             node_id = split[0][1:-1]
             for node_info in split:
                 if(node_info == "bounty"):
-                    bounty = getFloatValueFromTitle(split, node_info)
+                    bounty = getFloatValueFromTitle(split, node_info) #define a recompensa daquela cidade
 
                 if(node_info == "time_spent"):
-                    time_spent = getFloatValueFromTitle(split, node_info)
+                    time_spent = getFloatValueFromTitle(split, node_info) #define o tempo gasto naquela cidade
             node = Node(
                 node_id,
                 bounty,
@@ -75,7 +75,7 @@ def buildNode(file, graph):
 
             graph.addNode(node)
 
-def build_NodeEdgeList(graph, edge):
+def build_NodeEdgeList(graph, edge): #adiciona uma aresta na lista de arestas de um vértice específico
     for i in graph.nodes:
         if (i == edge.begin):
             i.edges.append(edge)
